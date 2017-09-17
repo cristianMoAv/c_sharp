89ae00f1df697eade541e70ccb94a2f5dba1a5bc
cristianMoAv <moav.cristian@gmail.com>
2017-09-17 13:50:53 -0300

    Initial Commit

---
+++ Hello_World/Hello_World/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+
+            //para que me lea lo ingresado por Console
+            Console.ReadKey();
+	    //AGREGO ESTE COMENTARIO COMO PRUEBA GIT
+        /*Podemos generar comentarios de 
+         * Varias líneas de esta manera
+         * Sirve para realizar parrafos de comentarios o tambien 
+         * Para comentar bloques de Código
+         */
+
+        }
+    }
+}
