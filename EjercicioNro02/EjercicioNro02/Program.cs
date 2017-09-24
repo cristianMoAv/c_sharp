4c3313b2285412aa55815dece84e0352a3e191b9
cristianMoAv <moav.cristian@gmail.com>
2017-09-24 04:21:16 -0300

    Ejr 02 completo

---
+++ EjercicioNro02/EjercicioNro02/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioNro02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float number; 
+            double risedNumberOne, risedNumberTwo;
+            
+            Console.WriteLine("Please type a number: ");
+
+            while(!float.TryParse(Console.ReadLine(),out number))
+            {
+                Console.WriteLine("ERRROR, ONLY TYPE A NUMBER");
+            }
+            while (number <= 0)
+            {
+                Console.WriteLine("ERRROR, THE NUMBER MUST BE MAYOR THAN 0, ENTRY A NEW NUMBER: ");
+
+                while (!float.TryParse(Console.ReadLine(), out number))
+                {
+                    Console.WriteLine("ERRROR, ONLY TYPE A NUMBER");
+                }
+            }
+            risedNumberOne = Math.Pow((double)number, 2);
+            risedNumberTwo = Math.Pow((double)number, 3);
+
+            Console.WriteLine("Elevate the number {0}. \nTo the squared is: {1}\nTo the third is {2}", number, risedNumberOne, risedNumberTwo);
+
+            Console.ReadKey();            
+        }
+    }
+}
