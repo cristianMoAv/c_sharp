9591d635b0e1ac15f60a346e43889000217186bd
cristianMoAv <moav.cristian@gmail.com>
2017-09-24 02:50:06 -0300

    Ejercicio Nro 1 completo

--- EjercicioNro01/EjercicioNro01/Program.cs
+++ EjercicioNro01/EjercicioNro01/Program.cs
@@ -8,23 +8,22 @@ namespace EjercicioNro01
 {
     class Program
     {
-        int auxiliarInt;
-        int max; 
-        int min;
-        int accumulator;
 
         static void Main(string[] args)
         {
+            int auxiliarInt, min, max, accumulator;
+            
             Console.WriteLine("INGRESE UN NÚMERO: ");
-            while(!int.TryParse(Console.ReadLine(), out auxiliarInt))
+
+            while (!int.TryParse(Console.ReadLine(), out auxiliarInt))
             {
                 Console.WriteLine("ERROR, INGRESAR SOLO VALORES NUMÉRICOS ENTEROS");
             }
             max = auxiliarInt;
             min = auxiliarInt;
-            accumulator = auxiliarInt; 
-             
-            for (int i= 0; i<4; i++)
+            accumulator = auxiliarInt;
+
+            for (int i = 0; i < 4; i++)
             {
                 Console.WriteLine("INGRESE UN NÚMERO: ");
 
@@ -32,19 +31,20 @@ namespace EjercicioNro01
                 {
                     Console.WriteLine("ERROR, INGRESAR SOLO VALORES NUMÉRICOS ENTEROS");
                 }
+
                 accumulator += auxiliarInt;
 
                 if (auxiliarInt > max)
                 {
                     max = auxiliarInt;
                 }
-                if(auxiliarInt < min)
+                if (auxiliarInt < min)
                 {
                     min = auxiliarInt;
                 }
             }
 
-            Console.WriteLine("El valor máximo es {0}, el minimo {1} y el promedio {2}", max, min, accumulator/5);
+            Console.WriteLine("El valor máximo es {0}, el minimo {1} y el promedio {2} ", max, min , accumulator/5 );
 
             Console.ReadKey();
             Console.Clear();
